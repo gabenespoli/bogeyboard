1ec4314a370870fa0d01f38140f4aba69b24a277
Gabe Nespoli <gabenespoli@gmail.com>
2026-08-26 16:49:47 -0400

    Drop Accounts and Scoring pages
    - Remove navigation entries and placeholder files
    - Scoring content lives on Overview; Accounts content lives in Data & Syncing

--- app_pages/accounts.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Accounts")
-
-st.info("Accounts & syncing has moved to the **Data & Syncing** page (Accounts tab).")
-if st.button("Go to Data & Syncing"):
-    st.switch_page("app_pages/data.py")
