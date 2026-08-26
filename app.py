6fd4f790892f41d18bc0420ecb97fc93dea10647
Gabe Nespoli <gabenespoli@gmail.com>
2026-08-26 16:55:15 -0400

    Move handicap tab to the end

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ st.set_page_config(page_title="Bogeyboard", layout="wide")
 page = st.navigation(
     [
         st.Page("app_pages/overview.py", title="Overview", icon=":material/dashboard:", default=True),
-        st.Page("app_pages/handicap.py", title="Handicap", icon=":material/military_tech:"),
         st.Page("app_pages/driving.py", title="Driving", icon=":material/track_changes:"),
         st.Page("app_pages/approach.py", title="Approach", icon=":material/flag:"),
         st.Page("app_pages/scrambling.py", title="Scrambling", icon=":material/crisis_alert:"),
         st.Page("app_pages/putting.py", title="Putting", icon=":material/flag:"),
         st.Page("app_pages/clubs.py", title="Clubs", icon=":material/golf_course:"),
+        st.Page("app_pages/handicap.py", title="Handicap", icon=":material/military_tech:"),
         st.Page("app_pages/data.py", title="Data & Syncing", icon=":material/table_chart:"),
     ]
 )
